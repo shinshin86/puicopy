bfc481a6641225607d6d53d5ffcedef4fc2c6c37
shinshin86 <beagles1986@gmail.com>
2023-09-13 13:47:48 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# puicopy
+
+※現在公開準備中です。公開完了次第、URLをこちらにも貼ります。
+
+AIイラスト・AIフォト・AIグラビア専用の投稿サイトである[『chichi-pui（ちちぷい）』](https://www.chichi-pui.com/)で公開されている呪文・プロンプトを、infotext形式でクリップボードにコピーすることができる機能を提供するChrome拡張機能です。
+
+## Usage
+Chrome拡張機能を有効にした状態、かつchichi-pui（ちちぷい）にログインした状態で、ちちぷいの投稿画像の閲覧画面にアクセスすると、`生成情報をコピー`ボタンが追加されます。  
+こちらをクリックすると、`infotext`形式でクリップボードにコピーされます。
+
+![『生成情報をコピー』ボタンに関するスクリーンショット画像](./screenshots/puicopy_usage01.png)
+
+こちらを[Stable Diffusion web UI](https://github.com/AUTOMATIC1111/stable-diffusion-webui)のプロンプト欄にコピーし、画面右側の矢印ボタンをクリックすることで、
+
+* Negative Prompt
+* Sampling Methods
+* Sampling Steps
+* Seed
+
+に値を貼り付けることが可能です。
+
+
+![『Stable Diffusion web UI』にinfotextをペーストした際のスクリーンショット画像](./screenshots/puicopy_usage02.png)
+
+## License
+
+MIT
---
+++ src/content-script.ts
@@ -0,0 +1,96 @@
+const ORIGINAL_BUTTON_TEXT = "生成情報をコピー";
+const COPYED_BUTTON_TEXT = "コピー完了！";
+
+const div = document.createElement("div");
+div.style.borderStyle = "solid";
+div.style.borderWidth = "0";
+div.style.boxSizing = "border-box";
+
+const button = document.createElement("button");
+button.innerHTML = ORIGINAL_BUTTON_TEXT;
+button.style.alignItems = "center";
+button.style.border = "1px solid #eaeae9";
+button.style.display = "flex";
+button.style.height = "40px";
+button.style.padding = "6px 11px";
+button.style.font = "#2c2d25";
+button.style.display = "inline-block";
+button.style.fontSize = "14px";
+button.style.fontWeight = "700";
+
+div.appendChild(button);
+
+// copy to clipboard
+button.addEventListener("click", () => {
+  const promptFields = document.querySelectorAll(
+    ".image-posts-detail__prompt-body",
+  );
+  const prompt = promptFields[0]?.textContent;
+  const negativePrompt = promptFields[1]?.textContent;
+
+  const parameters = document.querySelectorAll(
+    ".image-posts-detail__prompt-parameter-value",
+  );
+  const steps = parameters[0]?.textContent;
+  const scale = parameters[1]?.textContent;
+  const seed = parameters[2]?.textContent;
+  const sampler = parameters[3]?.textContent;
+  const strength = parameters[4]?.textContent;
+  const noise = parameters[5]?.textContent;
+
+  let copyText = prompt?.trim() || "";
+
+  if (negativePrompt) {
+    if (copyText) {
+      copyText += "\n";
+    }
+
+    copyText += "Negative prompt: " + negativePrompt?.trim();
+  }
+
+  if (copyText && (steps || scale || seed || sampler || strength || noise)) {
+    copyText += "\n";
+  }
+
+  if (steps) {
+    copyText += "Steps: " + steps + ", ";
+  }
+
+  if (scale) {
+    copyText += "CFG scale: " + scale + ", ";
+  }
+
+  if (seed) {
+    copyText += "Seed: " + seed + ", ";
+  }
+
+  if (sampler) {
+    copyText += "Sampler: " + sampler + ", ";
+  }
+
+  if (strength) {
+    // TODO
+  }
+
+  if (noise) {
+    // TODO
+  }
+
+  navigator.clipboard.writeText(copyText).then(() => {
+    button.innerText = COPYED_BUTTON_TEXT;
+    setTimeout(() => {
+      button.innerText = ORIGINAL_BUTTON_TEXT;
+    }, 1000);
+  }).catch((err) => {
+    console.error("Failed to copy text: ", err);
+  });
+});
+
+const targetSelector = ".image-posts-detail__actions";
+const taregtDiv = document.querySelector(targetSelector);
+
+if (taregtDiv?.firstChild) {
+  taregtDiv.insertBefore(div, taregtDiv.firstChild);
+} else if (taregtDiv) {
+  taregtDiv.appendChild(div);
+}
